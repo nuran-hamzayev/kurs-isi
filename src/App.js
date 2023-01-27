0e5f23104d7ec459f5d9c0b8b84d13af012d0d21
nuran-hamzayev <nuranhamzayev@gmail.com>
2023-01-27 04:01:33 -0800

    proykt hazirdir

--- src/App.js
+++ src/App.js
@@ -3,11 +3,42 @@ import 'bootstrap/dist/js/bootstrap'
 import { isCompositeComponentWithType } from 'react-dom/test-utils';
 import './App.css';
 
+
+
 // komponentler
 import Navbar from './components/Navbar';
 
 
 function App() {
+const say1 = document.querySelector('.aboutnumber_two')
+var timer;
+function saygac1() {
+    var sec = 0;
+    timer = setInterval(() => {
+        if (sec < 19) {
+            say1.innerHTML = sec + "+"
+            sec++
+        }
+    }, 1000);
+}
+saygac1()
+
+
+const say3 = document.querySelector('.aboutnumber')
+
+function saygac3() {
+    var sec = 0;
+    timer = setInterval(() => {
+        if (sec < 8000000001) {
+            say3.innerHTML = sec + "+"
+            sec++
+        }
+    }, 50);
+}
+saygac3()
+
+
+
   return (
     // REACT FRAGMNET
     <div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import "../navbar.css";
+
+
 // import logo from '../assets/svgs/logo.svg';
 
 function Navbar () {
   
   return (
-    <nav className="nav">
+    
+  <nav className="nav">
         <div>
-          <a class="nav__logo1" href="Britlexlogo">
+          <a class="nav__logo_one" href="Britlexlogo">
             <img src="https://nvyrsta.github.io/britlex/img/logo.png" alt="logo" />
           </a>
         </div>
@@ -57,66 +60,69 @@ function Navbar () {
       </button>
 
       
-        <h2 className="metin1">Learn <br /> Any Foreign Language</h2>
+        <h2 className="hero_title">Learn <br /> Any Foreign Language</h2>
         
-        <h3 className="metin2">With our teachers who write a program for <br /> 
+        <h3 className="hero_paragraf">With our teachers who write a program for <br /> 
 each student, you will be able to make your <br />
 first sketch after the first lesson.</h3>
-      
+   
+{/* CONTAINER */}
+
+
 
 <h2 className='title'>Skills</h2>
-  
-  <div className='skill_card'>
-    <div className='skill_img1'>
+
+  <div className='skill_one_card'>
+    <div className='skill_one_img'>
       <img src="https://nvyrsta.github.io/britlex/img/speaking.png" alt="image" />
     </div>
-    <h3 className='skills_name1'>Speaking</h3>
-    <p className='skill_paragraf1'>
+    <h3 className='skills_one_name'>Speaking</h3>
+    <p className='skill_one_paragraf'>
     Improve your English skills and confidence. <br /> Live classes and
     interactive lessons online. <br /> 20% extra free for a limited time only
     </p>
 
-    <p className='skill_paragraf2'>
+    <p className='skill_two_paragraf'>
     Learn English online and improve your <br /> skills through our high-quality courses <br /> and resources – all designed for adult <br /> language learners.
     </p>
 
     <button className='learnmore'>Learn More</button>
      
-     <div className='skill_card2'>
-      <div className='skill_img2'>
+     <div className='skill_two_card'>
+      <div className='skill_two_img'>
         <img src="https://nvyrsta.github.io/britlex/img/writing.png" alt="image" />
       </div>
-      <h3 className='skill_name2'>Writing</h3>
-      <p className='skill_paragraf3'>
+      <h3 className='skill_two_name'>Writing</h3>
+      <p className='skill_three_paragraf'>
       One of the most important and extensive <br />
 areas of natural science, the science that <br />
 studies substances, also their composition 
       </p>
-      <button className='learnmore2'>Learn More</button>
+      <button className='learnmore_two'>Learn More</button>
 
      </div>
 
-     <div className='skill_card3'>
-      <div className='skill_img3'>
+     <div className='skill_three_card'>
+      <div className='skill_three_img'>
         <img src="https://nvyrsta.github.io/britlex/img/reading.png" alt="image" />
       </div>
-      <h3 className='skill_name3'>Reading</h3>
-      <p className='skill_paragraf4'>perception and response actions of the user <br />
+      <h3 className='skill_three_name'>Reading</h3>
+      <p className='skill_four_paragraf'>perception and response actions of the user <br />
       resulting from the use and/or upcoming use <br /> of the product, system or service</p>
 
-      <button className='learnmore3'>Learn More</button>
+      <button className='learnmore_three'>Learn More</button>
      </div>
 
-     <div className='skill_card4'>
-      <div className='skill_img4'>
+     <div className='skill_four_card'>
+      <div className='skill_four_img'>
         <img src="https://nvyrsta.github.io/britlex/img/listening.png" alt="photo" />
       </div>
-      <h3 className='skill_name4'>Listening</h3>
-      <p className='skill_paragraf5'>Here you can find activities to practise <br />
+      <h3 className='skill_four_name'>Listening</h3>
+      <p className='skill_five_paragraf'>Here you can find activities to practise <br />
 your listening skills. Listening will help you <br />
 to improve your understanding </p>
 
-      <button className='learnmore4'>Learn More</button>
+      <button className='learnmore_four'>Learn More</button>
      </div>
 
      <div className='about_image'>
@@ -128,18 +134,18 @@ to improve your understanding </p>
     
     </div>
 
-    <div className='about_result1'>    
-    <p className='aboutnumber1'>800</p>
+    <div className='about_result_one'>    
+    <p className='aboutnumber'>800</p>
     <p className='pupils'>Pupils</p>
     </div>
 
-    <div className='about_result2'>
-      <p className='aboutnumber2'>18</p>
+    <div className='about_result_two'>
+      <p className='aboutnumber_two'>18</p>
       <p className='teachers'>Teachers</p>
     </div>
 
-    <div className='about_result3'>
-      <p className='aboutnumber3'>6</p>
+    <div className='about_result_three'>
+      <p className='aboutnumber_three'>6</p>
       <p className='foreign_languages'>Foreign languages</p>
     </div>
 
@@ -150,32 +156,38 @@ to improve your understanding </p>
       <img src="https://nvyrsta.github.io/britlex/img/self-study.png" alt="photo" />
     </div>
     <div className='pricing_card'></div>
-    <h2 className='pricing_name1'>Self-study online course</h2>
-    <p className='pricing_paragraf1'>Start learning English online in live classes <br /> with qualified EC teachers and students <br /> from all over the world.</p>
-    <p className='value'> £5.99</p>
-    <p className='value_paragraf'>per month</p>
+    <h2 className='pricing_name'>Self-study online course</h2>
+    <p className='pricing_paragraf'>Start learning English online in live classes <br /> with qualified EC teachers and students <br /> from all over the world.</p>
+    <p className='value_one'> £5.99</p>
+    <p className='value_one_paragraf'>per month</p>
 
      
-     <div className='pricing_img2'>
+     <div className='pricing_img_two'>
       <img src="	https://nvyrsta.github.io/britlex/img/online-classes.png" alt="photo" />
      </div>
-    <div className='pricing_card2'></div>
-    <h2 className='pricing_name2'>Live online classes</h2>
-    <p className='pricing_paragraf2'>Interactive group classes with expert <br />
+    <div className='pricing_card_two'></div>
+    <h2 className='pricing_name_two'>Live online classes</h2>
+    <p className='pricing_paragraf_two'>Interactive group classes with expert <br />
       teachers. Free 7-day trial <br /></p>
-      <p className='value2'> £12.99</p>
-      <p className='value_paragraf2'>per month</p>
+      <p className='value_two'> £12.99</p>
+      <p className='value_two_paragraf'>per month</p>
 
 
-      <div className='pricing_img3'>
+      <div className='pricing_img_three'>
         <img src="	https://nvyrsta.github.io/britlex/img/personal-tuition.png" alt="photo" />
       </div>
-      <div className='pricing_card3'></div>
-      <h2 className='pricing_name3'>Personal Tuition</h2>\
-      <p className='pricing_paragraf3'>Online one-to-one English tutoring – enjoy <br />
+      <div className='pricing_card_three'></div>
+      <h2 className='pricing_name_three'>Personal Tuition</h2>\
+      <p className='pricing_paragraf_three'>Online one-to-one English tutoring – enjoy <br />
       our first session for only $1</p>
+      <p className='value_three'> £20.99</p>
+      <p className='value_three_paragraf'>per month</p>
+      
+      
 
 
+      <footer>
+      
       <div className='contactus_img'>
         <img src="	https://nvyrsta.github.io/britlex/img/contact-us.png" alt="" />
       </div>
@@ -187,13 +199,13 @@ to improve your understanding </p>
     
     <hr className='hr'/>
 
-    <div className='britlexlogo2'>
+    <div className='britlexlogo_two'>
       <img src="https://nvyrsta.github.io/britlex/img/logo.png" alt="" />
     </div>
     <p className='endparagraf'>Terms and Conditions • Privacy Policy • Cookie Policy</p>
     
-    
-    
+    </footer>
+
     </nav>
         
   );
